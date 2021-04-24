3fea5da92f6e464c8b0c535bb3a17de8104a7897
Jim Barritt <jimb@thoughtworks.com>
2021-04-24 11:22:53 +0100

    Adding link to google maps

--- site/js/main.js
+++ site/js/main.js
@@ -1,23 +1,31 @@
 import {log} from './logging.js'
 
-function success(position) {
-    const latitude = position.coords.latitude
-    const longitude = position.coords.longitude
+function createCallback(fnCallBack) {
+    return function (position) {
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
 
-    log.info('You are located at Lat, Long (' + latitude + ', ' + longitude + ')')
+        log.info('You are located at Lat, Long (' + latitude + ', ' + longitude + ')')
+        fnCallBack(latitude, longitude)
+    }
 }
 
 function error() {
     log.error('Could not retrieve location')
 }
-function calculateGeoCode() {
-    log.info('Calculating geocode...')
 
-    if (!navigator.geolocation) {
-        log.error("Browser Does Not Support geolocation")
-    } else {
-        navigator.geolocation.getCurrentPosition(success, error , {enableHighAccuracy: true})
-    }
+function calculateGeoCode(fnCallBack) {
+        log.info('Calculating geocode...')
+
+        if (!navigator.geolocation) {
+            log.error("Browser Does Not Support geolocation")
+        } else {
+            navigator.geolocation.getCurrentPosition(
+                createCallback(fnCallBack),
+                error,
+                {enableHighAccuracy: true})
+        }
 }
 
-export { calculateGeoCode }
+
+export {calculateGeoCode}
