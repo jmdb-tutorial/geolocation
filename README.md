b1383f581d3894c44328a64955c59476e8f4535e
Jim Barritt <jimb@thoughtworks.com>
2021-04-24 10:55:58 +0100

    Adding geolocation

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 This web page demonstrates how to use geolocation from a website.
 
+https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
--- site/js/main.js
+++ site/js/main.js
@@ -1,7 +1,24 @@
 import {log} from './logging.js'
 
+function success(position) {
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+
+    log.info('You are located at Lat, Long (' + latitude + ', ' + longitude + ')')
+}
+
+function error() {
+    log.error('Could not retrieve location')
+}
 function calculateGeoCode() {
     log.info('Calculating geocode...')
+
+    if (!navigator.geolocation) {
+        log.error("Browser Does Not Support geolocation")
+    } else {
+
+        navigator.geolocation.getCurrentPosition(success, error)
+    }
 }
 
 export { calculateGeoCode }
